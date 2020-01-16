14226ce9a4ce03793859f3b86022ffc8bb44ad16
meowracle <aprilerd34@gmail.com>
2020-01-16 10:37:14 +0700

    chinh sua trang home, nut user, post list

--- src/app/user/home/home.component.ts
+++ src/app/user/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../_services/user.service';
+import {Post} from '../../interfaces/post';
+import {TokenStorageService} from '../_services/token-storage.service';
+import {PostService} from '../_services/post.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,7 @@ import { UserService } from '../_services/user.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  content: string;
+/*  content: string;
 
   constructor(private userService: UserService) { }
 
@@ -20,5 +23,19 @@ export class HomeComponent implements OnInit {
         this.content = JSON.parse(err.error).message;
       }
     );
+  }*/
+  postList: Post[] = [];
+  currentUser: any;
+
+  constructor(private token: TokenStorageService,
+              private postService: PostService) {
+  }
+
+  ngOnInit() {
+    this.currentUser = this.token.getUser();
+    this.postService
+      .getAllPosts()
+      .subscribe(next => (this.postList = next)
+        , error => (this.postList = []));
   }
 }
