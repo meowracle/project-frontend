1a21b5f7d085015e1433216cb54f895466476165
meowracle <aprilerd34@gmail.com>
2020-01-21 09:45:05 +0700

    chinh sua 1 ti

--- src/app/component/comments/comment-create/create-comment.component.ts
+++ src/app/component/comments/comment-create/create-comment.component.ts
@@ -1,15 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {CommentService} from '../comment.service';
 import {Comment} from '../../../interfaces/comment';
 import {TokenStorageService} from '../../../user/_services/token-storage.service';
 import {PostDetailComponent} from '../../post/post-detail/post-detail.component';
 import {Post} from '../../../interfaces/post';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-create-comment',
   templateUrl: './create-comment.component.html',
   styleUrls: ['./create-comment.component.css']
+})
+
+@Injectable({
+  providedIn: 'root'
 })
 export class CreateCommentComponent implements OnInit {
   createForm: FormGroup;
@@ -20,7 +25,8 @@ export class CreateCommentComponent implements OnInit {
   constructor(private fb: FormBuilder,
               private commentService: CommentService,
               private token: TokenStorageService,
-              private postDetail: PostDetailComponent) {
+              private postDetail: PostDetailComponent,
+              private router: Router) {
   }
 
   ngOnInit() {
